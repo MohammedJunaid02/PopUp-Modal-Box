c10a13e9881514f598fef421afadb609f2d44171
Mohammed Junaid <mohammedjunaidj08@gmail.com>
2023-10-26 07:58:54 +0530

    Created a simple Popup Modal Box using HTML,CSS and JS

---
+++ script.js
@@ -0,0 +1,9 @@
+// let pop=document.getElementById("popup");
+
+function openPopUp(){
+    popup.classList.add("open-popup");
+}
+
+function closePopUp(){
+    popup.classList.remove("open-popup");
+}
